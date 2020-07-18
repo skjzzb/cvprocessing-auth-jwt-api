4890e441f063757b502617b73988cb8d7c3bdfd3
Ishwari <itpednekar16@gmail.com>
2020-07-18 13:34:00 +0530

    List of roles and setRole

--- src/main/java/com/bezkoder/springjwt/controllers/ProfileController.java
+++ src/main/java/com/bezkoder/springjwt/controllers/ProfileController.java
@@ -1,6 +1,7 @@
 package com.bezkoder.springjwt.controllers;
 
-import java.util.Optional;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,15 +9,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bezkoder.springjwt.models.Profile;
+import com.bezkoder.springjwt.models.Role;
 import com.bezkoder.springjwt.models.User;
 import com.bezkoder.springjwt.repository.UserRepository;
 import com.bezkoder.springjwt.security.services.ProfileService;
+import com.bezkoder.springjwt.security.services.RolesService;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -28,6 +32,9 @@ public class ProfileController {
 
 	@Autowired
 	UserRepository userRepository;
+	
+	@Autowired
+	RolesService roleServiceObj;
 
 	@PutMapping("/profile/{userId}")
 	public ResponseEntity<?> editProfile(@RequestBody Profile profile, @PathVariable long userId) {
@@ -60,11 +67,28 @@ public class ProfileController {
 		return new ResponseEntity<>(userRepository.findById(userId), HttpStatus.OK);
 	}
 	
-
+	//http://localhost:8880/api/roles
 	@GetMapping("/user")
 	public ResponseEntity<?> getAllUsers() {
 		return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
 	}
 
+	@GetMapping("/roles")
+	public ResponseEntity<?> getListOfRoles(){
+		return new ResponseEntity<>(roleServiceObj.getAllRoles(), HttpStatus.OK);
+	}
+	
+	@PostMapping("/set-role/{userId}")
+	public ResponseEntity<?> setRole(@RequestBody Role role, @PathVariable long userId  ){
+		User u = userRepository.findById(userId).orElse(null);
+		System.out.println(u);
+			if(u != null) {
+			Set<Role> roleSet = new HashSet<Role>();
+			roleSet.add(role);
+			u.setRoles(roleSet);
+			userRepository.save(u);
+		}
+		return new ResponseEntity<>(u, HttpStatus.OK);
+	}
 
 }
---
+++ src/main/java/com/bezkoder/springjwt/security/services/RolesService.java
@@ -0,0 +1,9 @@
+package com.bezkoder.springjwt.security.services;
+
+import java.util.List;
+import com.bezkoder.springjwt.models.Role;
+
+public interface RolesService {
+	List<Role> getAllRoles();
+
+}
